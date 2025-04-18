ea202b90e0b92d6d7ef5d6415ae7e642f61e6e9c
drgorillamd <83670532+simon-something@users.noreply.github.com>
2025-04-19 00:02:37 +0200

    feat: init

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use alloy::{
+    primitives::address,
+    providers::{Provider, ProviderBuilder, WsConnect},
+    rpc::types::{BlockNumberOrTag, Filter},
+    sol,
+    sol_types::SolEvent,
+};
+use dotenv::dotenv;
+use eyre::Result;
+use futures_util::stream::StreamExt;
+
+sol! {
+    #[sol(rpc)]
+    "src/IUniswapV3PoolEvents.sol"
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+
+    let rpc_url = &std::env::var("RPC_URL").unwrap();
+    let ws = WsConnect::new(rpc_url);
+
+    let provider = ProviderBuilder::new().on_ws(ws).await?;
+    let pool = address!("88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640");
+
+    let filter = Filter::new()
+        .address(pool)
+        .from_block(BlockNumberOrTag::Latest);
+
+    let sub = provider.subscribe_logs(&filter).await?;
+    let mut stream = sub.into_stream();
+
+    while let Some(log) = stream.next().await {
+        println!("usdc eth: {log:?}");
+        match log.topic0() {
+            Some(&IUniswapV3PoolEvents::Swap::SIGNATURE_HASH) => {
+                let IUniswapV3PoolEvents::Swap {
+                    sender,
+                    recipient,
+                    amount0,
+                    amount1,
+                    sqrtPriceX96,
+                    liquidity,
+                    tick,
+                } = log.log_decode()?.inner.data;
+                println!(
+                    "Swap from {sender} to {recipient} of value {amount0} and {amount1} at {sqrtPriceX96} with {liquidity} at {tick}"
+                );
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
